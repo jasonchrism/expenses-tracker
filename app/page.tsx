14ebcc3828db01b959772b56cbac38534e70927b
Jason <jasoncm712@gmail.com>
2025-10-10 20:08:05 +0700

    Add navbar, guest, about and contact pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server"
 
-export default function HomePage() {
+export default async function HomePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />
+  }
+
   return (
     <div>
-      
+      Homepage
     </div>
   )
 }
